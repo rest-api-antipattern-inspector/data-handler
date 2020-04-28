2f34c1fd6b4a485d3a8971f494524d18f1762a9b
edvinpontuslarsson <el223na@student.lnu.se>
2020-04-28 17:17:49 +0200

    new json file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import { getData } from './data/data'
 import { getCorrelations, getCorrelationsString } from './lib/correlations'
+
 import fs from 'fs'
 
 // console.log(process.argv[2])
 
 const data = getData()
 const correlations = getCorrelations(data)
-const presentationString = getCorrelationsString(data.length, correlations)
+const presentation = getCorrelationsString(data.length, correlations)
 
-fs.writeFileSync('correlations.md', presentationString)
+fs.writeFileSync('correlations.md', presentation)
 
 console.log('Wrote stats to correlations.md')
