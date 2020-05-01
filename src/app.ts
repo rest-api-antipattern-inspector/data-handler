0d5566fffbbbb5a999db2263e63c7d1906afd743
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-01 13:35:12 +0200

    adds todos

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import fs from 'fs'
 
 // console.log(process.argv[2])
 
+// TODO try putting for all uris in one table, .xlsx
+// interested in correlations between antipatterns, not apis necessarily
+
 const data = getData()
 const correlations = getCorrelations(data)
 const presentation = getCorrelationsString(data.length, correlations)
