ef98d562b71541259fe1b7f6e7379da968dc2c99
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-01 13:45:39 +0200

    can create folder if not exists & file

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import fs from 'fs'
 // console.log(process.argv[2])
 
 // TODO try putting for all uris in one table, .xlsx
+// or start w. creating one for each api
 // interested in correlations between antipatterns, not apis necessarily
 
 const data = getData()
@@ -13,5 +14,9 @@ const correlations = getCorrelations(data)
 const presentation = getCorrelationsString(data.length, correlations)
 
 fs.writeFileSync('correlations.md', presentation)
+
+!fs.existsSync('./test') && fs.mkdirSync('./test')
+
+fs.writeFileSync('./test/test.txt', 'hej då')
 
 console.log('Wrote stats to correlations.md')
