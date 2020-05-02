dcc04efef2df146ef93ff32740831bfa9d6d3b47
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-02 11:51:53 +0200

    almost done w arr code

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,9 @@ const data = getData()
 const sourceR = getRDataCode(data)
 console.log(sourceR)
 
-// const correlations = getCorrelations(data)
-// const presentation = getCorrelationsMDString(data.length, correlations)
+const correlations = getCorrelations(data)
+const presentation = getCorrelationsMDString(data.length, correlations)
 
-// fs.writeFileSync('./correlation-data/correlations.md', presentation)
+fs.writeFileSync('./correlation-data/correlations.md', presentation)
 
-// console.log('Wrote stats to ./correlation-data/correlations.md')
+console.log('Wrote stats to ./correlation-data/correlations.md')
--- src/lib/createRCode.ts
+++ src/lib/createRCode.ts
@@ -1,27 +1,59 @@
 import IMeta from '../interfaces/IMeta'
+import IRData from '../interfaces/IRData'
 
 export const getRDataCode = (metas: IMeta[]): string => {
-  const data = {}
+  const data: IRData = {
+    apis: new Set(),
+    antipatternTypes: new Set(),
+    apData: {},
+  }
 
-  // TODO can make reusaqble func for both design & linguistic
   metas.forEach((m) => {
-    Object.keys(m.designAntipatterns).forEach((ap) => {
-      if (m.designAntipatterns[ap] /** Antipattern */) {
-        if (!data[`${m.api}_${ap}`] /** Create if doesn't exist */) {
-          data[`${m.api}_${ap}`] = 0
-        }
-
-        // increments count of that antipattern for that api
-        data[`${m.api}_${ap}`]++
-      }
-    })
+    appendAntipatternData(data, m, 'designAntipatterns')
+    appendAntipatternData(data, m, 'linguisticAntipatterns')
   })
 
-  let sourceR = ''
+  let RVars = '# antipattern count variables:\n\n'
+  let RMatrixValues = ''
 
-  Object.keys(data).forEach((key) => {
-    sourceR += `${key} = ${data[key]}\n`
+  Object.keys(data['apData']).forEach((key, i) => {
+    RVars += `${key}=${data['apData'][key]}\n`
+
+    RMatrixValues += `${key}, `
+
+    if (i === data.antipatternTypes.size) {
+      RMatrixValues + '\n'
+    }
   })
 
-  return sourceR
+  // removes last comma with space
+  RMatrixValues = RMatrixValues.substring(0, RMatrixValues.length - 2)
+
+  let apCalculation = `#antipatterns
+
+mydata <- matrix(c(${RMatrixValues}), nrow=${data.apis.size},
+ncol=${data.antipatternTypes.size},byrow = TRUE)
+
+chisq.test(mydata)
+`
+
+  return `${RVars}\n\n${apCalculation}`
+}
+
+const appendAntipatternData = (
+  data: IRData,
+  m: IMeta,
+  antipatternType: string
+) => {
+  data.apis.add(m.api)
+
+  Object.keys(m[antipatternType]).forEach((ap) => {
+    data.antipatternTypes.add(ap)
+
+    if (!data['apData'][`${m.api}_${ap}`] /** Create if doesn't exist */) {
+      data['apData'][`${m.api}_${ap}`] = 0
+    }
+
+    data['apData'][`${m.api}_${ap}`] += m[antipatternType][ap] ? 1 : 0
+  })
 }
