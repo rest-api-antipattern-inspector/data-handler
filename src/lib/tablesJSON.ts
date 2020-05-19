9338ad69a1f0bee49144f4f8408d538f8c293210
edvinpontuslarsson <el223na@student.lnu.se>
2020-05-19 23:22:44 +0200

    giant json

--- src/lib/tablesJSON.ts
+++ src/lib/tablesJSON.ts
@@ -14,45 +14,60 @@ export const writeTablesJSON = (metas: IMeta[]) => {
   const tables: ITable[] = []
 
   metas.forEach((m) => {
-    // TODO put in function, for all combinations
-    Object.keys(m.linguisticAntipatterns).forEach((lKey) => {
-      Object.keys(m.designAntipatterns).forEach((dKey) => {
-        if (!existsTable(tables, lKey, dKey)) {
-          tables.push({
-            keyOne: lKey,
-            keyTwo: dKey,
-            a: 0,
-            b: 0,
-            c: 0,
-            d: 0,
-          })
-        }
+    appendData(tables, m, 'linguisticPatterns', 'designAntipatterns')
+    appendData(tables, m, 'designPatterns', 'linguisticAntipatterns')
+    appendData(tables, m, 'linguisticAntipatterns', 'designAntipatterns')
 
-        const t = getTables(tables, lKey, dKey)[0]
-
-        if (m.linguisticAntipatterns[lKey] && m.designAntipatterns[dKey]) {
-          t.a++
-        }
-
-        if (m.linguisticAntipatterns[lKey] && !m.designAntipatterns[dKey]) {
-          t.b++
-        }
-
-        if (!m.linguisticAntipatterns[lKey] && m.designAntipatterns[dKey]) {
-          t.c++
-        }
-
-        if (!m.linguisticAntipatterns[lKey] && !m.designAntipatterns[dKey]) {
-          t.d++
-        }
-      })
-    })
+    // vice versa
+    appendData(tables, m, 'designAntipatterns', 'linguisticPatterns')
+    appendData(tables, m, 'linguisticAntipatterns', 'designPatterns')
+    appendData(tables, m, 'designAntipatterns', 'linguisticAntipatterns')
   })
 
   fs.writeFileSync(
     './data-files/design-antipatterns/tables.json',
     JSON.stringify(tables)
   )
+}
+
+const appendData = (
+  tables: ITable[],
+  m: IMeta,
+  typeOne: string,
+  typeTwo: string
+) => {
+  Object.keys(m[typeOne]).forEach((keyOne) => {
+    Object.keys(m[typeTwo]).forEach((keyTwo) => {
+      if (!existsTable(tables, keyOne, keyTwo)) {
+        tables.push({
+          keyOne: keyOne,
+          keyTwo: keyTwo,
+          a: 0,
+          b: 0,
+          c: 0,
+          d: 0,
+        })
+      }
+
+      const t = getTables(tables, keyOne, keyTwo)[0]
+
+      if (m[typeOne][keyOne] && m[typeTwo][keyTwo]) {
+        t.a++
+      }
+
+      if (m[typeOne][keyOne] && !m[typeTwo][keyTwo]) {
+        t.b++
+      }
+
+      if (!m[typeOne][keyOne] && m[typeTwo][keyTwo]) {
+        t.c++
+      }
+
+      if (!m[typeOne][keyOne] && !m[typeTwo][keyTwo]) {
+        t.d++
+      }
+    })
+  })
 }
 
 const getTables = (tables: ITable[], keyOne: string, keyTwo: string) =>
